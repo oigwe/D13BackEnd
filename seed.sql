7c82ef230831ca9526214d959bed1f4ac955a90f
ositaigwe@pursuit.org <j0h@example.com>
2019-03-17 12:45:01 -0400

    changed column names for multiple tables

--- seed.sql
+++ seed.sql
@@ -4,68 +4,71 @@ CREATE DATABASE d13backend;
 \c d13backend;
 
 CREATE TABLE users (
-    id VARCHAR UNIQUE PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
-    name VARCHAR NULL,
+    nameOfUser VARCHAR NULL,
     username VARCHAR UNIQUE NULL,
     title VARCHAR NULL,
     profileUrl VARCHAR NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
+
 
 CREATE TABLE endorsements (
-    id SERIAL PRIMARY KEY UNIQUE, 
-    person_endorsing_id VARCHAR REFERENCES users(id) NOT NULL,
-    person_being_endorsed_id VARCHAR REFERENCES user(id) NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL, 
+    personEndorsingId INT REFERENCES users(id) NOT NULL,
+    personBeingEndorsedId INT REFERENCES users(id) NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE posts (
-    id SERIAL PRIMARY KEY UNIQUE,
-    userid VARCHAR REFERENCES users(id) NOT NULL, 
-    image_url VARCHAR NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
+    userId INT REFERENCES users(id) NOT NULL, 
+    imageUrl VARCHAR NULL,
     caption VARCHAR NOT NULL,
     numberOfComments INT NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE comments (
-    id SERIAL PRIMARY KEY UNIQUE, 
-    post_id INT REFERENCES posts(id) NOT NULL,
-    userid VARCHAR REFERENCES users(id) NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL, 
+    postId INT REFERENCES posts(id) NOT NULL,
+    userId INT REFERENCES users(id) NOT NULL,
     commentText TEXT NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
-CREATE TABLE Followers (
-    id SERIAL PRIMARY KEY UNIQUE,
-    person_following_id VARCHAR REFERENCES users(id) NOT NULL, 
-    person_being_followed VARCHAR REFERENCES users(id) NOT NULL,
+CREATE TABLE followers (
+    id SERIAL PRIMARY KEY NOT NULL,
+    personFollowingId INT REFERENCES users(id) NOT NULL, 
+    personBeingFollowed INT REFERENCES users(id) NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-) 
+); 
+
 
 CREATE TABLE newspapers (
-    id SERIAL PRIMARY KEY UNIQUE,
+    id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-) 
+);
 
-CREATE TABLE  shows (
-    id SERIAL PRIMARY KEY UNIQUE,
+CREATE TABLE  tvShows (
+    id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR NOT NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
-CREATE TABLE userPreference (
-    id SERIAL PRIMARY KEY UNIQUE, 
-    userid  VARCHAR REFERENCES users(id),
+CREATE TABLE userpreference (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    userId INT REFERENCES users(id) NOT NULL,
     newstype_1 INT REFERENCES newspapers(id) NULL,
     newstype_2 INT REFERENCES newspapers(id) NULL,
-    newstype_2 INT REFERENCES newspapers(id) NULL,
-    tvtype_1 INT REFERENCES tv(id) NULL,
-    tvtype_2 INT REFERENCES tv(id) NULL,
-    tvtype_3 INT REFERENCES tv(id) NULL,
+    newstype_3 INT REFERENCES newspapers(id) NULL,
+    tvtype_1 INT REFERENCES tvShows(id) NULL,
+    tvtype_2 INT REFERENCES tvShows(id) NULL,
+    tvtype_3 INT REFERENCES tvShows(id) NULL,
     createdAt TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
+
 
 
