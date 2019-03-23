eb529919bb1e7749351cf3d08cae37a748c36160
ositaigwe@pursuit.org <j0h@example.com>
2019-03-23 08:21:07 -0400

    adds jest and coverall dependencies

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-const port = 9000;
-
-const commentsRouter = require('./routes/comments');
-const endorsementsRouter = require('./routes/endorsements');
-const followersRouter = require('./routes/followers');
-const newspapersRouter = require('./routes/newspapers');
-const postsRouter = require('./routes/posts');
-const tvShowsRouter = require('./routes/tvshows');
-const userPreferenceRouter = require('./routes/userPreference');
-const usersRouter = require('./routes/users');
-
-
-// MIDDLEWARE NEEDED
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-// routes
-app.use('/comments', commentsRouter);
-app.use('/endorsements', endorsementsRouter);
-app.use('/followers', followersRouter);
-app.use('/newspapers', newspapersRouter);
-app.use('/posts', postsRouter);
-app.use('/tvshows', tvShowsRouter);
-app.use('/userPreference', userPreferenceRouter);
-app.use('/users', usersRouter);
-
-
-
-app.use((err, req, res, next) => {
-  res.status(400).json({error: err.toString()});
-});
+const {app,} = require('./app')
 
 app.listen(port, () => {
   console.log('D13 API is running on Port: '+port);
--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')({});
-const db = pgp('postgres://localhost/d13backend');
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost/d13backend');
 
 module.exports = {
     db,
